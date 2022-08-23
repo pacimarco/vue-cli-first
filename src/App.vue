3a5773276d719998981f310375ab572d4970ce5f
pacimarco <m.paci1987@gmail.com>
2022-08-23 19:29:39 +0200

    title con vue cli

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <my-title />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import myTitle from '@/components/myTitle.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    myTitle
   }
-}
+  }
+
 </script>
 
 <style lang="scss">
---
+++ src/components/myTitle.vue
@@ -0,0 +1,13 @@
+<template>
+<h2>La mia prima app con Vue CLI!</h2>
+</template>
+
+<script>
+export default {
+name: 'myTitle'
+}
+</script>
+
+<style>
+
+</style>
